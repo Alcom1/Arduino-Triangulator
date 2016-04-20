d8f5f49db3152f71584a388882d421c2a8aa5d79
Alexander Bronner <acbronn@gmail.com>
2016-04-20 01:20:20 -0400

    Flipped inLocs positions. Code cleaning and comments.

--- Sound_Triangulator/Sound_Triangulator.ino
+++ Sound_Triangulator/Sound_Triangulator.ino
@@ -1,53 +1,49 @@
 #include "vect.h"
 
-const int inPins[] = {A0, A1, A2};
-const int outPins[] = {3, 5, 6, 9};
+const int inPins[] = {A0, A1, A2};  //Pins for analog input (potes, mics)
+const int outPins[] = {3, 5, 6, 11}; //Pins for LED outputs
 
+//Vector location of the inputs
 Vect inLocs[] = {
-  {-0.866, -0.5},
+  {0.866, -0.5},
   {0, 1},
-  {0.866, -0.5}};
+  {-0.866, -0.5}};
 
+//Vector location of the outputs
 Vect outLocs[] = {
   {0.707, -0.707},
   {0.707, 0.707},
   {-0.707, 0.707},
   {-0.707, -0.707}};
 
-const float outAngles[] = {
-  -45,
-  45,
-  135,
-  225};
-
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
-
+//Setup
 void setup()
 {
     // initialize serial communications at 9600 bps:
     Serial.begin(9600);
 }
 
+//Loop
 void loop()
 {
-    Vect inTots = {0, 0};
+    Vect inTots = {0, 0}; //Total positional vector of analog inputs.
 
+    //Add inputs to total.
     for(int i = 0; i < 3; i++)
     {
-        sensorValue = analogRead(inPins[i]);
-        outputValue = map(sensorValue, 0, 1023, 0, 108);
-        inTots += inLocs[i] * mapFloat(sensorValue, 0, 1023, 0, 1);
+        inTots += inLocs[i] * mapFloat(analogRead(inPins[i]), 0, 1023, 0, 1);
     }
 
+    //If total is not close to zero, light up the LEDs in its direction.
     if(inTots.getLength() > 0.05)
     {
+        //For each LED, write to it.
         for(int i = 0; i < 4; i++)
         {
             analogWrite(
               outPins[i],
-              max(
-                mapFloat(
+              max(          //Ignore anything less than 0
+                mapFloat(   //Map LED angle to output value.
                   outLocs[i].getAngleBetween(inTots),
                   0,
                   PI / 2,
@@ -56,6 +52,7 @@ void loop()
                 0));
         }
     }
+    //Else, write nothing to the LEDs
     else
     {
         for(int i = 0; i < 4; i++)
@@ -67,15 +64,9 @@ void loop()
     delay(2);
 }
 
-float dist(Vect a, Vect b)
+//Map method for floats.
+float mapFloat(float x, float inMin, float inMax, float outMin, float outMax)
 {
-    return sqrt(
-      (a.x - b.x) *(a.x - b.x) +
-      (a.y - b.y) *(a.y - b.y));
-}
-
-float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
--- Sound_Triangulator/vect.h
+++ Sound_Triangulator/vect.h
@@ -1,39 +1,45 @@
 struct Vect
 {
-    float x;
-    float y;
-    
+    float x;  //X pos
+    float y;  //Y pos
+
+    //Add vectors
     Vect operator +(const Vect& other)
     {
         Vect ret = {x + other.x, y + other.y};
         return ret;
     }
-    
+
+    //Add assign vectors
     Vect operator +=(const Vect& other)
     {
         x += other.x;
         y += other.y;
     }
-    
+
+    //Multiply this by a constant.
     Vect operator *(const float& other)
     {
         Vect ret = {x * other, y * other};
         return ret;
     }
-    
+
+    //Get the dot product between this and another vector.
+    float getDot(Vect other)
+    {
+        return x * other.x + y * other.y;
+    }
+
+    //Return the length of this vector
     float getLength()
     {
         return sqrt(x * x + y * y);
     }
 
+    //Get the unsigned angle between this vector and another.
     float getAngleBetween(Vect other)
     {
         return acos(getDot(other) / (getLength() * other.getLength()));
-    }
-
-    float getDot(Vect other)
-    {
-        return x * other.x + y * other.y;
     }
 };
 
